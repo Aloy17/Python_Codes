a2ca17af42f6e050a889e9ae45c05d3ec03802f3
Ryane Bose <144593183+Aloy17@users.noreply.github.com>
2025-02-14 10:48:03 +0530

    Added a README text

--- Basic/Dice.py
+++ Basic/Dice.py
@@ -1,3 +1,15 @@
+'''
+# Dice Roller Game
+
+# Description
+    Uses the random module to choose between numbers 1-6 and displays the number
+    Doesn't require any external modules
+
+# Usage
+    After running the file, type "y" to roll the dice
+    Type "n" after the dice is rolled to stop the program
+'''
+
 import random
 numbers = [1,2,3,4,5,6]
 while True:
